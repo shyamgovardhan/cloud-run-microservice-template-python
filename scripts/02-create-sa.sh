42fb9153d53a4e5081fe72ba1169086a221d7f8f
Shyam Govardhan <shyamg@google.com>
2023-08-02 11:30:35 +1000

    create-sa

--- scripts/02-create-sa.sh
+++ scripts/02-create-sa.sh
@@ -1,29 +1,30 @@
 #!/usr/bin/env bash
 
-
-initfile="./00-init.sh"
+initfile="$(dirname $0)/00-init.sh"
 [[ ! -f ${initfile} ]] && echo "${initfile}: file not found!" && exit
 . ${initfile}
 
-gcloud iam service-accounts create ${SERVICE_ACCOUNT}
+createSa() {
+    sa=$1
+    status=$(gcloud iam service-accounts list --filter=chugai-token-creator-sa1 --format="value(disabled)")
+    [ "${status}" == "False" ]] && echo "${sa}: service account exists!" && return
+    gcloud iam service-accounts create ${SERVICE_ACCOUNT_ID}
+}
 
-gcloud projects add-iam-policy-binding $PROJECT_ID \
-    --member="serviceAccount:${SERVICE_ACCOUNT}@$PROJECT_ID.iam.gserviceaccount.com" \
-    --role="roles/iam.serviceAccountTokenCreator"
-gcloud projects add-iam-policy-binding $PROJECT_ID \
-    --member="serviceAccount:${SERVICE_ACCOUNT}@$PROJECT_ID.iam.gserviceaccount.com" \
-    --role="roles/run.invoker"
+grantPerm() {
+    sa=$1
+    role=$2
+    echo "sa: <$sa>"
+    echo "role: <$role>"
+    gcloud projects add-iam-policy-binding ${PROJECT_ID} \
+        --member="serviceAccount:${sa}" \
+        --role="roles/${role}"
+}
 
-gcloud projects add-iam-policy-binding $PROJECT_ID \
-    --member="serviceAccount:$PROJECT_NUMBER@cloudbuild.gserviceaccount.com" \
-    --role="roles/iam.serviceAccountTokenCreator"
 
-gcloud projects add-iam-policy-binding $PROJECT_ID \
-    --member="serviceAccount:$PROJECT_NUMBER@cloudbuild.gserviceaccount.com" \
-    --role="roles/run.admin"
-gcloud projects add-iam-policy-binding $PROJECT_ID \
-    --member="serviceAccount:$PROJECT_NUMBER@cloudbuild.gserviceaccount.com" \
-    --role="roles/iam.serviceAccountUser"
-gcloud projects add-iam-policy-binding $PROJECT_ID \
-    --member="serviceAccount:$PROJECT_NUMBER@cloudbuild.gserviceaccount.com" \
-    --role="roles/artifactregistry.repoAdmin"
+grantPerm ${SERVICE_ACCOUNT} "iam.serviceAccountTokenCreator"
+grantPerm ${SERVICE_ACCOUNT} "run.invoker"
+grantPerm ${CLOUD_BUILD_SERVICE_ACCOUNT} "iam.serviceAccountTokenCreator"
+grantPerm ${CLOUD_BUILD_SERVICE_ACCOUNT} "run.admin"
+grantPerm ${CLOUD_BUILD_SERVICE_ACCOUNT} "iam.serviceAccountUser"
+grantPerm ${CLOUD_BUILD_SERVICE_ACCOUNT} "artifactregistry.repoAdmin"
